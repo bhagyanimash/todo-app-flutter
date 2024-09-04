9180b4623499a4a06d344a51c126e401a32cb0fa
bhagyanimash <bhagyanimash2000@gmail.com>
2024-09-04 14:43:49 +0530

    update:screens to pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:todo/components/todolist.dart';
-import 'Screens/home_page.dart';
+import 'pages/home_page.dart';
 
 void main() {
   runApp(const ToDoApp());
